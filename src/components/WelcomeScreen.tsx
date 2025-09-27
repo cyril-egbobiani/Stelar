6106e95dedac73b28f4a7d7dcaedd46663d3501d
cyril-egbobiani <116189048+cyril-egbobiani@users.noreply.github.com>
2025-09-27 22:56:27 +0100

    built welcome screen

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -229,7 +229,7 @@ function WelcomeScreen({ onNavigate }: WelcomeScreenProps) {
 
         <div
           ref={buttonsContainerRef}
-          className="flex flex-row gap-4 justify-center"
+          className="flex flex-col md:flex-row gap-4 justify-center border-4 px-9 md:px-0"
         >
           <button
             ref={primaryButtonRef}
